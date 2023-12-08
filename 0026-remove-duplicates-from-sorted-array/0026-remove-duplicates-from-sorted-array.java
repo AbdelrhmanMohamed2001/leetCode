dced2c0cd205d8449c50d6e85be2f2b8ad1809be
Abdelrhman Mohamed Sayed Ahmed <108559069+AbdelrhmanMohamed2001@users.noreply.github.com>
2023-12-08 15:18:45 +0200

    Time: 1 ms (97.17%), Space: 43.9 MB (38.65%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Stack;
+
+//Trace
+
+/*nums= [0,0,1,1,1,2,2,3,3,4]
+i=0, j=1  num[i]=0     num[j]=0   exit
+i=0, j=2  num[i]=0     num[j]=1
+i=1, j=3  num[i]=1      num[j]=1   exit
+i=1, j=4  num[i]=1      num[j]=1   exit
+i=1, j=5  num[i]=1      num[j]=2
+i=2, j=6  num[i]=2      num[j]=2    exit
+i=2, j=7  num[i]=2      num[j]=3
+i=3, j=8  num[i]=3      num[j]=3    exit
+i=3, j=9  num[i]=3      num[j]=4
+i=4, j=0  num[i]=      num[j]=
+*/
+ 
+class Solution {
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length==0)
+            return 0;
+        int i=0;
+        for (int j = i+1; j < nums.length; j++) {
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
+
+        }
+        return i+1;
+
+    }
+
+
+
+    public static void main (String[]args){
+        int[] nums= {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums));
+
+
+
+        }
+
+
+}
