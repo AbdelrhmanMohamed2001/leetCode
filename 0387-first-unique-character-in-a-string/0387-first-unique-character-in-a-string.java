18b3cddf2d369912c889da51b6aab1a044877d9a
Abdelrhman Mohamed Sayed Ahmed <108559069+AbdelrhmanMohamed2001@users.noreply.github.com>
2023-12-12 14:18:11 +0200

    Update 0387-first-unique-character-in-a-string.java

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,3 +1,15 @@
+/*
+Input: s = "leetcode"  l-0 && 0-?-->0
+Output: 0
+
+Input: s = "loveleetcode"  l,0,4// o,1,9// v,2,2
+Output: 2
+
+charAt--> returns the character at the specified index in a string
+indexOf--> returns the position of the first occurrence of character(s) in a string
+lastIndexOf--> return the position of the last occurrence of character(s) in a string
+ */
+
 class Solution {
         public static int firstUniqChar(String s) {
         for (int i = 0; i < s.length(); i++) {
